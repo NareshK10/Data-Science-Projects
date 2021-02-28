1813e1e051317b7e54f4ec5b876045198259271c
NareshK10 <69425480+NareshK10@users.noreply.github.com>
2021-02-28 09:39:21 -0800

    Add files via upload

---
+++ Black Friday Sales Using ML/test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from joblib import dump, load
+load('blackfriday.joblib')
+
+
+# In[2]:
+
+
+load
+
+
+# In[4]:
+
+
+from joblib import dump, load
+dump(Regressor,'blackfriday.joblib')
+
+
+# In[ ]:
+
+
+
+
